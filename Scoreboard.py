bb17de5448871a039260927ec154e3c5bdbf393f
BricoBC <barajas.c.bruno.n@gmail.com>
2023-05-29 23:15:49 -0600

    add: function obtener_valores_entries

--- Scoreboard.py
+++ Scoreboard.py
@@ -9,6 +9,28 @@ window.geometry("650x500")  # anchoXaltura
 def close_program():
     window.destroy()
 
+def obtener_valores_entries():
+    count = 0
+    for inst, i, j, k in zip(arr_inst, arr_i, arr_j, arr_k):
+        txt_inst = inst.get()
+        txt_i = i.get()
+        txt_j = j.get()
+        txt_k = k.get()
+
+
+        lbl = tk.Label(window, text=txt_inst)
+        lbl.grid(row=15+count, column=0)
+        lbl = tk.Label(window, text=txt_i)
+        lbl.grid(row=15+count, column=1)
+        lbl = tk.Label(window, text=txt_j)
+        lbl.grid(row=15+count, column=2)
+        lbl = tk.Label(window, text=txt_k)
+        lbl.grid(row=15+count, column=3)
+
+        count = count+1
+        print(count)
+        
+
 lbl_to_user = tk.Label(window, text = 'Usa en INST: LF - ADDF - MULTF - DIVF - SUBF para tus instrucciones')
 lbl_to_user.grid(row=0, column=0, columnspan=3)
 lbl_inst = tk.Label(window, text = "INST")
@@ -20,68 +42,28 @@ lbl_j.grid(row=1, column=2)
 lbl_k = tk.Label(window, text = "k")
 lbl_k.grid(row=1, column=3)
 
-#first row
-cj_txt_inst = tk.Entry(window)
-cj_txt_inst.grid(row=2, column=0)
-cj_txt_i = tk.Entry(window)
-cj_txt_i.grid(row=2, column=1)
-cj_txt_j = tk.Entry(window)
-cj_txt_j.grid(row=2, column=2)
-cj_txt_k = tk.Entry(window)
-cj_txt_k.grid(row=2, column=3)
+arr_inst = []
+arr_i = []
+arr_j = []
+arr_k = []
 
-#second row
-cj_txt_inst = tk.Entry(window)
-cj_txt_inst.grid(row=3, column=0)
-cj_txt_i = tk.Entry(window)
-cj_txt_i.grid(row=3, column=1)
-cj_txt_j = tk.Entry(window)
-cj_txt_j.grid(row=3, column=2)
-cj_txt_k = tk.Entry(window)
-cj_txt_k.grid(row=3, column=3)
-
-#third row
-cj_txt_inst = tk.Entry(window)
-cj_txt_inst.grid(row=4, column=0)
-cj_txt_i = tk.Entry(window)
-cj_txt_i.grid(row=4, column=1)
-cj_txt_j = tk.Entry(window)
-cj_txt_j.grid(row=4, column=2)
-cj_txt_k = tk.Entry(window)
-cj_txt_k.grid(row=4, column=3)
-
-#fourth row
-cj_txt_inst = tk.Entry(window)
-cj_txt_inst.grid(row=5, column=0)
-cj_txt_i = tk.Entry(window)
-cj_txt_i.grid(row=5, column=1)
-cj_txt_j = tk.Entry(window)
-cj_txt_j.grid(row=5, column=2)
-cj_txt_k = tk.Entry(window)
-cj_txt_k.grid(row=5, column=3)
-
-#fifth row
-cj_txt_inst = tk.Entry(window)
-cj_txt_inst.grid(row=6, column=0)
-cj_txt_i = tk.Entry(window)
-cj_txt_i.grid(row=6, column=1)
-cj_txt_j = tk.Entry(window)
-cj_txt_j.grid(row=6, column=2)
-cj_txt_k = tk.Entry(window)
-cj_txt_k.grid(row=6, column=3)
-
-#sixth row
-cj_txt_inst = tk.Entry(window)
-cj_txt_inst.grid(row=7, column=0)
-cj_txt_i = tk.Entry(window)
-cj_txt_i.grid(row=7, column=1)
-cj_txt_j = tk.Entry(window)
-cj_txt_j.grid(row=7, column=2)
-cj_txt_k = tk.Entry(window)
-cj_txt_k.grid(row=7, column=3)
+for i in range(2,8):
+    #rows
+    cj_txt_inst = tk.Entry(window)
+    cj_txt_inst.grid(row=i, column=0)
+    arr_inst.append(cj_txt_inst);
+    cj_txt_i = tk.Entry(window)
+    cj_txt_i.grid(row=i, column=1)
+    arr_i.append(cj_txt_i)
+    cj_txt_j = tk.Entry(window)
+    cj_txt_j.grid(row=i, column=2)
+    arr_j.append(cj_txt_j)
+    cj_txt_k = tk.Entry(window)
+    cj_txt_k.grid(row=i, column=3)
+    arr_k.append(cj_txt_k)
 
 # buttons to close and nex operation
-boton_mostrar = tk.Button(window, text="Ok")
+boton_mostrar = tk.Button(window, text="Ok", command=obtener_valores_entries)
 boton_mostrar.grid(row=10, column=0)
 
 boton_ocultar = tk.Button(window, text="Cerrar programa", command=close_program)
